d891a5cb63f5afb69f0dadcd66ad2589fc6830a8
HilaryOkello <71628910+HilaryOkello@users.noreply.github.com>
2024-05-28 17:23:56 +0300

    Add files via upload

--- playground/flagtrial/main.go
+++ playground/flagtrial/main.go
@@ -1,1 +1,27 @@
-cd
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type customFlagValue string
+
+func (c *customFlagValue) String() string {
+	return string(*c)
+}
+
+func (c *customFlagValue) Set(value string) error {
+	*c = customFlagValue(value)
+	return nil
+}
+
+func main() {
+	var color string
+
+	flag.StringVar(&color, "color", "black", "Set the color")
+
+	flag.Parse()
+	
+	fmt.Println("Color:", color)
+}

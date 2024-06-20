94cc7527fd8029f6d3e6bb37dbf36f1c00cb1cd5
hilaokello <hilaryokello091@gmail.com>
2024-06-20 13:05:32 +0300

    message

--- playground/atoibase/main.go
+++ playground/atoibase/main.go
@@ -1,42 +1,61 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func anyBaseToDecimal(number string, base string) int {
+func main() {
+	fmt.Println(AtoiBase("125", "0123456789"))
+	fmt.Println(AtoiBase("1111101", "01"))
+	fmt.Println(AtoiBase("7D", "0123456789ABCDEF"))
+	fmt.Println(AtoiBase("uoi", "choumi"))
+	fmt.Println(AtoiBase("bbbbbab", "-ab"))
+}
+
+func AtoiBase(s string, base string) int {
+	if r := validateBase(base); r < 1 {
+		return r
+	}
 	var result int
-	var power int
 	var digitIndex int
-	for i := len(number) - 1; i >= 0; i-- {
-		digit := number[i]
+	var p int
+	for i := len(s) - 1; i >= 0; i-- {
+		digit := s[i]
 		track := false
 		for index, char := range base {
 			if char == rune(digit) {
-				digitIndex = index
 				track = true
+				digitIndex = index
 				break
 			}
 		}
 		if !track {
-			fmt.Println(string(number[i]), "not in", base)
 			return 0
 		}
-		result += digitIndex * Power(len(base), power)
-		power++
+		result += digitIndex * power(len(base), p)
+		p++
 	}
 	return result
 }
 
-func Power(num, n int) int {
-	if n == 0 {
+func power(num, p int) int {
+	if p == 0 {
 		return 1
 	}
-	return num * Power(num, n-1)
+	return num * power(num, p-1)
 }
 
-func main() {
-	number :="10"
-	base := "01"
-	fmt.Println("Decimal value:", anyBaseToDecimal(number, base))
+func validateBase(base string) int {
+	if len(base) < 2 {
+		return 0
+	}
+	baseMap := map[rune]bool{}
+	for _, char := range base {
+		if char == '-' || char == '+' {
+			return 0
+		}
+		if baseMap[char] {
+			return 0
+		}
+		baseMap[char] = true
+	}
+	return 1
 }
